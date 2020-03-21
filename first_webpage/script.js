cc872f33b8509322fdf3770ab7da7b8d59def743
Adeola Ogunanya <ogunsanya.adeola71@gmail.com>
2020-03-21 13:59:16 +0100

    second commit

---
+++ first_webpage/script.js
@@ -0,0 +1,17 @@
+
+let name = "Adeola Ogunsanya";
+
+let courses = ["HTML", "CSS", "JavaScript"];
+
+console.log(name);
+console.log(courses);
+
+
+
+for (let i = 1; i < 200; i+=2){
+
+    if (courses.length % 2 !== 0) {
+        console.log(i);
+    }
+    
+}
